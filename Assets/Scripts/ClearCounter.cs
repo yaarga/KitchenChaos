6abf330c0419192b56124d9e55f1fdbfa78258ee
yaarga <yaargadot@gmail.com>
2024-01-17 10:46:46 +0200

    4:23

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class ClearCounter : BaseCounter{
 
-
-
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-
-
 
     public override void Interact(Player player){
         if (!HasKitchenObject()){
