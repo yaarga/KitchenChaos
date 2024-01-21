17dd9b5dfa77dba3e133b469c2cf43c46f7a4102
yaarga <yaargadot@gmail.com>
2024-01-21 17:41:53 +0200

    5:05

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CuttingCounter : BaseCounter {
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+    public class OnProgressChangedEventArgs : EventArgs {
+        public float progressNormalized;
+    }
+    public event EventHandler OnCut;
 
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
-
+    [SerializeField] private CuttingRecpieSO[] cuttingRecpieSOArray;
+    private int cuttingProgress;
     public override void Interact(Player player)
     {
         if (!HasKitchenObject())
@@ -14,7 +21,18 @@ public class CuttingCounter : BaseCounter {
             if (player.HasKitchenObject())
             {
                 //player is carring smthing
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                if (HasRecpieWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    //Drop it
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                    cuttingProgress = 0;
+
+                    CuttingRecpieSO cuttingRecpieSO = GetCuttingRecpieSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+
+                    OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs {
+                        progressNormalized = (float)cuttingProgress / cuttingRecpieSO.cuttingProgressMax
+                    });
+                }
             }
             else
             {
@@ -38,16 +56,46 @@ public class CuttingCounter : BaseCounter {
     }
     public override void InteractAlternate(Player player)
     {
-        if (HasKitchenObject())
-        {
-            //There is a KitchenObject here
-            GetKitchenObject().DestroySelf();
+        if (HasKitchenObject() && HasRecpieWithInput(GetKitchenObject().GetKitchenObjectSO())){
+        //There is a kitchenObject here and it can be cut
+            cuttingProgress++;
+            OnCut?.Invoke(this, EventArgs.Empty);
+            CuttingRecpieSO cuttingRecpieSO = GetCuttingRecpieSOWithInput(GetKitchenObject().GetKitchenObjectSO());
 
-            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
-            
+            OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs
+            {
+                progressNormalized = (float)cuttingProgress / cuttingRecpieSO.cuttingProgressMax
+            });
 
+            if (cuttingProgress >= cuttingRecpieSO.cuttingProgressMax){
+                KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+                GetKitchenObject().DestroySelf();
+                KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+            }
         }
+    }
+    private bool HasRecpieWithInput(KitchenObjectSO inputKitchenObjectSO){
+        CuttingRecpieSO cuttingRecpieSO = GetCuttingRecpieSOWithInput(inputKitchenObjectSO);
+        return cuttingRecpieSO != null;
 
+    }
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO){
+        CuttingRecpieSO cuttingRecpieSO = GetCuttingRecpieSOWithInput(inputKitchenObjectSO);
+        if (cuttingRecpieSO != null){
+           return cuttingRecpieSO.output;
+        }
+        else{
+            return null;
+        }
+        
+    }
+    private CuttingRecpieSO GetCuttingRecpieSOWithInput(KitchenObjectSO inputKitchenObjectSO){
+        foreach (CuttingRecpieSO cuttingRecpieSO in cuttingRecpieSOArray){
+            if (cuttingRecpieSO.input == inputKitchenObjectSO){
+                return cuttingRecpieSO;
+            }
+        }
+        return null;
     }
 }
 
