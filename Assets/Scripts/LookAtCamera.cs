5991994727cee3f9c68cdcb99337891b47feb2d7
yaarga <yaargadot@gmail.com>
2024-01-21 18:35:01 +0200

    5:12:30

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour{
+    private enum Mode {
+        LookAt,
+        LookAtInverted,
+    }
+    [SerializeField] private Mode mode;
+
+    private void LateUpdate(){
+        switch (mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + dirFromCamera);
+                break;
+        }
+
+    }
+}
