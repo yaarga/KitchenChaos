b7be18036ed0983ebf8aafffbd4bbf7050649020
yaarga <yaargadot@gmail.com>
2024-01-23 19:20:27 +0200

    5:19-end of trashcounter

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -6,6 +6,8 @@ public class LookAtCamera : MonoBehaviour{
     private enum Mode {
         LookAt,
         LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
     }
     [SerializeField] private Mode mode;
 
@@ -18,6 +20,12 @@ public class LookAtCamera : MonoBehaviour{
             case Mode.LookAtInverted:
                 Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
                 transform.LookAt(transform.position + dirFromCamera);
+                break;
+            case Mode.CameraForward:
+                transform.forward= Camera.main.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward= -Camera.main.transform.forward;
                 break;
         }
 
